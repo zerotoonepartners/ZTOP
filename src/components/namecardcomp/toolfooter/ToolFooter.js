ab713d8c41ff260d0e45b65c533f3abd73581deb
ahn kyung rok <akrnote97@gmail.com>
2021-08-10 17:18:43 +0900

    a

--- src/components/namecardcomp/toolfooter/ToolFooter.js
+++ src/components/namecardcomp/toolfooter/ToolFooter.js
@@ -4,57 +4,20 @@ import car from '../../../static/images/car.png';
 import home from '../../../static/images/home.png';
 import share from '../../../static/images/share.png';
 import phone from '../../../static/images/phone.png';
-<<<<<<< HEAD
-import { useEffect,useContext } from 'react';
-import { ZtopContext } from '../../../context/ztop';
-=======
 import { ZtopContext } from '../../../context/ztop';
 import { useContext } from 'react';
->>>>>>> 30a048d23373a45884570491ebecaccaa93a132a
 import Kakaolink from './kakaolink';
 
 function ToolFooter({ no }) {
-  const { namecard_ } = useContext(ZtopContext);
-  const [kp, setKp] = useState();
+  const { namecard_,setNamecardTarget } = useContext(ZtopContext);
   useEffect(() => {
-<<<<<<< HEAD
-    try{
-    window.Kakao.init('1b4391ecab97342f8952423eba09a979');
-    }catch(e){}
-  }, []);
-
-  const shareKakao = () => {
-    window.Kakao.Link.sendDefault({
-      objectType: 'feed',
-      content: {
-        title: '타이틀',
-        description: '내용!',
-        imageUrl: '',
-        link: {
-          mobileWebUrl: '123',
-          androidExecParams: 'test',
-        },
-      },
-      buttons: [
-        {
-          title: '웹으로 이동',
-          link: {
-            mobileWebUrl: '123',
-          },
-        },
-      ],
-    });
-    console.log(window.Kakao.Link);
-  };
-=======
-    namecard_ &&
-      namecard_.map((item, idx) => {
+    namecard_.data &&
+      namecard_.data.map((item, idx) => {
         if (no == item.id) {
-          setKp(item.kakao_picture);
+          setNamecardTarget(item.kakao_picture);
         }
       });
   }, []);
->>>>>>> 30a048d23373a45884570491ebecaccaa93a132a
   return (
     <div className="toolFooterWrapper">
       <div className="footerContentWrapper">
@@ -109,18 +72,10 @@ function ToolFooter({ no }) {
         >
           <div className="shareBtn">
             <img src={share} alt="" />
-<<<<<<< HEAD
             <Kakaolink/>
             <span>공유하기</span>
-            {/* <button onClick={shareKakao}>카카오톡 공유하기</button> */}
-          </a>
-        </div>
-=======
-            <Kakaolink kp={kp} />
-            {/* <span>공유하기</span> */}
           </div>
         </a>
->>>>>>> 30a048d23373a45884570491ebecaccaa93a132a
       </div>
     </div>
   );
