065aed4bad8addc30f85e4e0e84734e0d834e3c1
ChoiHeekyeong <gmlrud9701@naver.com>
2021-08-11 15:14:51 +0900

    .

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,11 +22,13 @@ function About({ match }) {
           <Newheader />
           <div className="aboutPageWrapper">
             <div className="aboutSlogan">
-              <span>스타트업 성장의 시작을 함께하겠습니다.</span>
+              <span>About ZeroToOnePartners</span>
             </div>
             <div className="aboutLanding"></div>
             <div className="aboutInfoDesc">
+              <span className="strong">스타트업의 성장 과정을 함께하겠습니다.</span>
               <span>
+                제로투원파트너스는 <br />
                 다년간 축척해 온 전문성과 노하우를 바탕으로 아이디어 단계부터
                 초기단계 (Seed~Series A)에 해당하는 스타트업 유망주들의 성공적인
                 비즈니스 구축을 지원하며, 국내뿐 아니라 해외 진출을 위한 글로벌
@@ -34,11 +36,12 @@ function About({ match }) {
                 <br /> 현재보다 더 나은 가치를 창조하고, 보다 나은 세상을 만들어
                 갈 수 있는 목표를 가진 기업에게 더 높은 성장의 발판을 제공하고자
                 언제나 노력하고 있습니다. <br />
-                <br /> 다양한 분야의 전문가 및 네트워크를 통하여 스타트업에게 더
-                빠르게 나아갈 수 있는 액셀러레이팅을 통해 스타트업의 목표를 이룰
+                <br /> 다양한 분야의 전문가 및 네트워크를 통해 스타트업에게 더
+                빠르게 나아갈 수 있는 엑셀러레이팅으로 스타트업의 목표를 이룰
                 수 있도록 함께 하겠습니다.
               </span>
             </div>
+            <div className="aboutLine"></div>
             <div className="teamTitle">
               <span>Team</span>
             </div>
@@ -48,15 +51,17 @@ function About({ match }) {
                   return (
                     <>
                       <Link to={`/namecard/${item.id}`}>
-                        <div className="avatarCardWrapper">
-                          <img src={item.picture} alt="" className="cardImg" />
-                          <div className="cardInfoWrapper">
-                            <div className="nameWrapper">
-                              <span className="mainName">{item.name} </span>
-                              <span className="subName">{item.sub_name}</span>
-                            </div>
-                            <div className="statusWrapper">
-                              <span className="statusSpan">{item.status}</span>
+                        <div className="gridWrapper">
+                          <div className="avatarCardWrapper">
+                            <img src={item.picture} alt="" className="cardImg" />
+                            <div className="cardInfoWrapper">
+                              <div className="nameWrapper">
+                                <span className="mainName">{item.name} </span>
+                                <span className="subName">{item.sub_name}</span>
+                              </div>
+                              <div className="statusWrapper">
+                                <span className="statusSpan">{item.status}</span>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -76,6 +81,7 @@ function About({ match }) {
                   return (
                     <>
                       <Link to={`/namecard/${item.id}`}>
+                      <div className="gridWrapper">
                         <div className="avatarCardWrapper">
                           <img src={item.picture} alt="" className="cardImg" />
                           <div className="cardInfoWrapper">
@@ -88,6 +94,7 @@ function About({ match }) {
                             </div>
                           </div>
                         </div>
+                      </div>
                       </Link>
                     </>
                   );
@@ -104,15 +111,17 @@ function About({ match }) {
                   return (
                     <>
                       <Link to={`/namecard/${item.id}`}>
-                        <div className="avatarCardWrapper">
-                          <img src={item.picture} alt="" className="cardImg" />
-                          <div className="cardInfoWrapper">
-                            <div className="nameWrapper">
-                              <span className="mainName">{item.name} </span>
-                              <span className="subName">{item.sub_name}</span>
-                            </div>
-                            <div className="statusWrapper">
-                              <span className="statusSpan">{item.status}</span>
+                      <div className="gridWrapper">
+                          <div className="avatarCardWrapper">
+                            <img src={item.picture} alt="" className="cardImg" />
+                            <div className="cardInfoWrapper">
+                              <div className="nameWrapper">
+                                <span className="mainName">{item.name} </span>
+                                <span className="subName">{item.sub_name}</span>
+                              </div>
+                              <div className="statusWrapper">
+                                <span className="statusSpan">{item.status}</span>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -132,15 +141,17 @@ function About({ match }) {
                   return (
                     <>
                       <Link to={`/namecard/${item.id}`}>
-                        <div className="avatarCardWrapper">
-                          <img src={item.picture} alt="" className="cardImg" />
-                          <div className="cardInfoWrapper">
-                            <div className="nameWrapper">
-                              <span className="mainName">{item.name} </span>
-                              <span className="subName">{item.sub_name}</span>
-                            </div>
-                            <div className="statusWrapper">
-                              <span className="statusSpan">{item.status}</span>
+                      <div className="gridWrapper">
+                          <div className="avatarCardWrapper">
+                            <img src={item.picture} alt="" className="cardImg" />
+                            <div className="cardInfoWrapper">
+                              <div className="nameWrapper">
+                                <span className="mainName">{item.name} </span>
+                                <span className="subName">{item.sub_name}</span>
+                              </div>
+                              <div className="statusWrapper">
+                                <span className="statusSpan">{item.status}</span>
+                              </div>
                             </div>
                           </div>
                         </div>
