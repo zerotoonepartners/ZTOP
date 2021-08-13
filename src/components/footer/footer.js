2b5361368ca1925f41dcbdf0c42620cd8cc7df78
ChoiHeekyeong <gmlrud9701@naver.com>
2021-08-13 15:05:59 +0900

    init

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,13 +12,13 @@ function Footer() {
         <span>
           (주)제로투원파트너스 <br /> 서울특별시 서초구 서초중앙로22길117 <br />
           대표 : 김경태 | 사업자등록번호 : 288-87-00902 <br /> TEL) 02-6228-5100
-          | E-Mail) info@ztop.kr
+          | E-Mail) info@ztop.kr<br/>
+          COPYRIGHTS © 2021, ZEROTOONEPARTNERS<br/>ALL rights reserved
         </span>
+
       </div>
 
-      <div className="footerCopyWrapper">
-        <span>COPYRIGHTS © 2021, ZEROTOONEPARTNERS ALL rights reserved</span>
-      </div>
+
 
     </div>
   );
