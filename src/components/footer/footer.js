836891be1fb77317294d011d88c829456e2dcbe3
ChoiHeekyeong <gmlrud9701@naver.com>
2021-08-13 17:48:12 +0900

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './footer.scss';
+import instaIcon from '../../static/images/instaIcon.png';
+import blogIcon from '../../static/images/blogIcon.png';
+import newsIcon from '../../static/images/newsletterIcon.png';
+
 
 function Footer() {
   return (
@@ -15,7 +19,11 @@ function Footer() {
           | E-Mail) info@ztop.kr<br/>
           COPYRIGHTS © 2021, ZEROTOONEPARTNERS<br/>ALL rights reserved
         </span>
-
+      </div>
+      <div className="iconWrapper">
+        <img src={blogIcon} className="blogIcon" onClick={() => window.open('https://blog.naver.com/sjscjr', '_blank')}></img>
+        <img src={instaIcon} className="instaIcon" onClick={() => window.open('https://www.instagram.com/zerotoonepartners/', '_blank')}></img>
+        <img src={newsIcon} className="newsIcon" onClick={() => window.open('https://page.stibee.com/subscriptions/82706', '_blank')}></img>
       </div>
 
 
