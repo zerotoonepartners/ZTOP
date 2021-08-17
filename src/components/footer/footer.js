d665222845d9a439a0d73a6e3cdd3b11bf0f54ac
ChoiHeekyeong <gmlrud9701@naver.com>
2021-08-17 13:33:54 +0900

    fooooter

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './footer.scss';
-import instaIcon from '../../static/images/instaIcon.png';
-import blogIcon from '../../static/images/blogIcon.png';
-import newsIcon from '../../static/images/newsletterIcon.png';
+import instaIcon from '../../static/images/newInstaIcon.png';
+import blogIcon from '../../static/images/newBlogIcon.png';
+import newsIcon from '../../static/images/newNewsletterIcon.png';
 
 
 function Footer() {
   return (
     <div className="footerWrapper">
       <div className="footerLogoWrapper">
-        <span>ZeroToOne Partners</span>
+        <span>ZeroToOne<br/>Partners</span>
       </div>
 
       <div className="footerContentWrapper">
         <span>
-          (주)제로투원파트너스 <br /> 서울특별시 서초구 서초중앙로22길117 <br />
-          대표 : 김경태 | 사업자등록번호 : 288-87-00902 <br /> TEL) 02-6228-5100
-          | E-Mail) info@ztop.kr<br/>
-          COPYRIGHTS © 2021, ZEROTOONEPARTNERS<br/>ALL rights reserved
+          (주)제로투원파트너스 &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; 대표 : 김경태<br />
+          서울특별시 서초구 서초중앙로22길117 4층 &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;TEL) 02-6228-5100 &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; E-Mail) info@ztop.kr  <br />
+          사업자등록번호 : 288-87-00902 &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; 통신판매업신고번호: 제 2019-서울서초-3027 호<br/><br/>
+          <span className="copyrights">COPYRIGHTS © 2021, ZEROTOONEPARTNERS ALL rights reserved</span>
         </span>
       </div>
       <div className="iconWrapper">
