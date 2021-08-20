1bab55fd5b495a3fcaf9407eca043f2d776e5f85
JJH <naturalflavor1007@naver.com>
2021-08-20 15:36:47 +0900

    kakao

--- src/components/namecardcomp/toolfooter/kakaolink.js
+++ src/components/namecardcomp/toolfooter/kakaolink.js
@@ -1,45 +1,42 @@
 import React, { useEffect, useContext } from 'react';
 import { ZtopContext } from '../../../context/ztop';
 
-function Kakaolink() {
+function Kakaolink({ item }) {
   const { namecardTarget } = useContext(ZtopContext);
+
   useEffect(() => {
     if (namecardTarget && namecardTarget.length > 1) {
+      console.log(namecardTarget);
       try {
         window.Kakao.init('d149effdaec276eadf4b088d938de0f2');
       } catch (e) {}
-      // try {
-      window.Kakao.Link.sendDefault({
-        container: '#kakao_share',
-        objectType: 'feed',
-        content: {
-          title: '타이틀',
-          description: '내용!',
-          imageUrl: namecardTarget,
-          link: {
-            mobileWebUrl: '',
-            webUrl: window.location.href,
-          },
-        },
-        social: {
-          likeCount: 286,
-          commentCount: 45,
-          sharedCount: 845,
-        },
-        buttons: [
-          {
-            title: '제로투원파트너스',
+      try {
+        window.Kakao.Link.createDefaultButton({
+          container: '#kakao_share',
+          objectType: 'feed',
+          content: {
+            title: `제로투원파트너스 ${item.name}입니다.`,
+            description: '',
+            imageUrl: namecardTarget,
             link: {
-              mobileWebUrl: window.location.href,
+              mobileWebUrl: '',
               webUrl: window.location.href,
             },
           },
-        ],
-      });
-      // } catch (e) {}
+
+          buttons: [
+            {
+              title: '제로투원파트너스',
+              link: {
+                mobileWebUrl: window.location.href,
+                webUrl: window.location.href,
+              },
+            },
+          ],
+        });
+      } catch (e) {}
     }
   }, [namecardTarget]);
-
   return <div></div>;
 }
 
