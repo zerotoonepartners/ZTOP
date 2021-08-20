4d88247a5c388d882d8edb7c8d5264ce96a73983
JJH <naturalflavor1007@naver.com>
2021-08-20 11:32:22 +0900

    namecard

--- src/components/namecardcomp/toolfooter/ToolFooter.js
+++ src/components/namecardcomp/toolfooter/ToolFooter.js
@@ -13,68 +13,78 @@ function ToolFooter({ no }) {
     namecard_.data &&
       namecard_.data.map((item, idx) => {
         if (no == item.id) {
+          console.log(item);
           setNamecardTarget(item.kakao_picture);
         }
       });
   }, []);
   return (
-    <div className="toolFooterWrapper">
-      <div className="footerContentWrapper">
-        <div className="companyTitle">
-          <span>(주) 제로투원파트너스</span>
-        </div>
-        <div className="companyInfo">
-          <div className="infoItem">
-            <span className="infoSpan">
-              <span className="strong">A. </span> (06631) 서울시 서초구
-              서초중앙로22길 117, 4층 (서초동, 그라비타스)
-            </span>
-          </div>
-          <div className="infoItem">
-            <span className="infoSpan">
-              <span className="strong">T. </span>
-              02-6228-5100
-            </span>
-          </div>
-          <div className="infoItem">
-            <span className="infoSpan">
-              <span className="strong">M. </span>
-              010-1234-5678
-              <span className="strong strongMargin">E. </span>
-              info@ztop.kr
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="toolWrapper">
-        <div className="toolItem">
-          <a href="tel:02-6228-5100">
-            <img src={phone} alt="" />
-          </a>
-          <span>전화상담</span>
-        </div>
+    <>
+      {namecard_.data &&
+        namecard_.data.map((item, index) => {
+          if (no == item.id) {
+            return (
+              <div className="toolFooterWrapper">
+                <div className="footerContentWrapper">
+                  <div className="companyTitle">
+                    <span>(주) 제로투원파트너스</span>
+                  </div>
+                  <div className="companyInfo">
+                    <div className="infoItem">
+                      <span className="infoSpan">
+                        <span className="strong">A. </span> (06631) 서울시
+                        서초구 서초중앙로22길 117, 4층 (서초동, 그라비타스)
+                      </span>
+                    </div>
+                    <div className="infoItem">
+                      <span className="infoSpan">
+                        <span className="strong">T. </span>
+                        02-6228-5100
+                      </span>
+                    </div>
+                    <div className="infoItem">
+                      <span className="infoSpan">
+                        <span className="strong">M. </span>
+                        {item.phone_num}
+                        <span className="strong strongMargin">E. </span>
+                        info@ztop.kr
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="toolWrapper">
+                  <div className="toolItem">
+                    <a href="tel:02-6228-5100">
+                      <img src={phone} alt="" />
+                    </a>
+                    <span>전화상담</span>
+                  </div>
 
-        <div className="toolItem">
-          <a href="https://zerotoonepartners.com/">
-            <img src={home} alt="" />
-          </a>
-          <span>홈페이지</span>
-        </div>
+                  <div className="toolItem">
+                    <a href="https://zerotoonepartners.com/">
+                      <img src={home} alt="" />
+                    </a>
+                    <span>홈페이지</span>
+                  </div>
 
-        <a
-          id="kakao_share"
-          href="javascript:sendLink()"
-          className="toolItem lastChild"
-        >
-          <div className="shareBtn">
-            <img src={share} alt="" />
+                  <a
+                    id="kakao_share"
+                    href="javascript:sendLink()"
+                    className="toolItem lastChild"
+                  >
+                    <div className="shareBtn">
+                      <img src={share} alt="" />
 
-            <Kakaolink />
-            <span>공유하기</span>
-          </div>
-        </a>
-      </div>
-    </div>
+                      <Kakaolink />
+                      <span>공유하기</span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            );
+          }
+        })}
+    </>
   );
 }
 
